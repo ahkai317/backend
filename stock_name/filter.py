01985d525f76a930d2cf84ade41177e2a0406835
zentropy <zentropy1229@gmail.com>
2022-05-16 22:56:05 +0800

    20220516

--- stock_name/filter.py
+++ stock_name/filter.py
@@ -3,7 +3,7 @@ import django_filters
 
 class StockFilter(django_filters.rest_framework.FilterSet):
     stock = django_filters.CharFilter(
-        field_name='stock', lookup_expr='startswith')
+        field_name='stock', lookup_expr='iexact')
     sort = django_filters.OrderingFilter(
         fields=['stockName', 'stockName', '-stock', 'stock', 'industry', '-industry', 'updated'])
     stockName = django_filters.CharFilter(
