4c6d32a469303aa8ee7ec7ea3f2b704d7dd0d0db
zentropy <zentropy1229@gmail.com>
2022-05-24 19:21:30 +0800

    20220524

--- backend/settings.py
+++ backend/settings.py
@@ -31,8 +31,6 @@ AUTH_USER_MODEL = 'user_info.UserInfo'
 AUTHENTICATION_BACKENDS = ['user_info.backendAuth.CaseInsensitiveModelBackend']
 
 CORS_ALLOWED_ORIGINS = [
-    'http://35.78.225.253',
-    'http://35.78.225.253:3000',
     'http://127.0.0.1:8081',
     'http://localhost:8081',
     'http://127.0.0.1:8080',
